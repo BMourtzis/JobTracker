8abce53c55afd4889021e82762cf5d8aaeb43a79
BMourtzis <vasilis.mourtzis@outlook.com>
2016-06-13 02:25:22 +1000

    Initial commit by setting up some development modules needed

---
+++ README.md
@@ -0,0 +1,3 @@
+JobTracker
+
+I wil use electron and electro edge to create a Job Tracking software with nice visuals.
---
+++ renderer.js
@@ -0,0 +1,6 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+window.$ = window.jQuery = require('jquery');
+window.coreJS = require('core-js');
+window.bootstrap = require('bootstrap');

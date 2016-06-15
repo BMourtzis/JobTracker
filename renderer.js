6ad93acaccdb52befa2f5139a4d7421ed9d90423
BMourtzis <vasilis.mourtzis@outlook.com>
2016-06-15 22:23:24 +1000

    Fixed all bugs with the development enviroment

--- renderer.js
+++ renderer.js
@@ -1,6 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-window.$ = window.jQuery = require('jquery');
+$ = window.jQuery = require('jquery');
 window.coreJS = require('core-js');
 window.bootstrap = require('bootstrap');
+window.sqlite3 = require('sqlite3');
+window.sequelize = require('sequelize');
+var database = require('./app/scripts/database.js');

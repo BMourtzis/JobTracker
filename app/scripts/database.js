c77aa9c52575738338ac4828d787cd4df75f024e
BMourtzis <vasilis.mourtzis@outlook.com>
2016-06-17 01:58:07 +1000

    Learned how to use sequelize and made the first table

--- app/scripts/database.js
+++ app/scripts/database.js
@@ -1,43 +1,81 @@
-var seq = new sequelize(null, null, null, {
+'use strict'
+
+window.connStr = new sequelize(null, null, null, {
     host: 'localhost',
     dialect: 'sqlite',
     pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
+        max: 5,
+        min: 0,
+        idle: 10000
     },
     storage: 'app/db/jobs.db'
-  });
+});
 
-  seq
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(function (err) {
-    console.log('Unable to connect to the database:', err);
-  });
+// window.connStr
+// .authenticate()
+// .then(function(err) {
+//   console.log('Connection has been established successfully.');
+// })
+// .catch(function (err) {
+//   console.log('Unable to connect to the database:', err);
+// });
 
-  var Client = seq.define('client', {
+var Client = connStr.define('client', {
     id: {
-      type: sequelize.INTEGER,
-      primaryKey: true,
-      field: 'id'
+        type: sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        field: 'id'
     },
-    firstname: {
-      type: sequelize.STRING,
-      field: 'firstname'
+    firstName: {
+        type: sequelize.STRING,
+        field: 'firstname',
+        allowNull: false
     },
-    lastname: {
-      type: sequelize.STRING,
-      field: 'lastname'
+    lastName: {
+        type: sequelize.STRING,
+        field: 'lastname',
+        allowNull: false
     },
     businessName: {
-      type:  sequelize.STRING,
-      field: 'businessName'
+        type: sequelize.STRING,
+        field: 'businessname',
+        allowNull: false
+    },
+    shortName: {
+        type: sequelize.STRING(3),
+        field: 'shortname',
+        unique: true,
+        allowNull: false
+    },
+    Address: {
+      type: sequelize.STRING(150),
+      allowNull: false,
+      field: 'address'
+    },
+    Email: {
+      type: sequelize.STRING(50),
+      field: 'email',
+      validate: {
+        isEmail: true
+      }
+    },
+    Phone: {
+      type: sequelize.INTEGER(10),
+      field: 'phone'
     }
-  });
+});
 
-  Client.findOne().then(function (client) {
-    console.log(client);
+// Client.sync({force: true})
+
+var c = Client.create({
+  firstName: 'Bill',
+  lastName: 'Mourtzis',
+  businessName: 'BM Software Pty Ltd',
+  shortName: 'VCC',
+  Address: '28 Jackson Cres, Pennant Hills, 2120',
+  Email: 'mK@gmail.com',
+  Phone: '2106127281',
+}).then(function(client) {
+  console.log(client.get({plain: true}));
 });
